7098d17f51ab078e78d7814a4818ff36f0352352
Dhanya-07 <105242133+Dhanya-07@users.noreply.github.com>
2023-01-09 20:34:05 +0530

    Create JavaJNI.java

---
+++ JavaJNI.java
@@ -0,0 +1,23 @@
+public class JavaJNI {  // Save as HelloJNI.java
+   static {
+      System.loadLibrary("load"); // Load native library hello.dll (Windows) or libhello.so (Unixes)
+                                   //  at runtime
+                                   // This library contains a native method called sayHello()
+   }
+ 
+   // Declare an instance native method sayHello() which receives no parameter and returns void
+   private native int JNI_callSumFunc(int n1, int n2);
+   private native int JNI_callDiff(int n1, int n2);
+   private native int JNI_callProdFunc(int n1, int n2);
+   private native int JNI_callDivFunc(int n1, int n2);
+ 
+   // Test Driver
+   public static void main(String[] args) {
+      System.out.println("Arithmetic Operations");
+      //The functions are from C# with C language acting as a bridge
+      System.out.println("The value of sum obtained in java code: "+new JavaJNI().JNI_callSumFunc(100, 20));  // Create an instance and invoke the native method
+      System.out.println("The value of Difference obtained in java code: "+new JavaJNI().JNI_callDiff(20, 7)); 
+      System.out.println("The value of Product obtained in java code: "+new JavaJNI().JNI_callProdFunc(10, 5)); 
+      System.out.println("The value of Division obtained in java code: "+new JavaJNI().JNI_callDivFunc(1135, 5)); 
+   }
+}
